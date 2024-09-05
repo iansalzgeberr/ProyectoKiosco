097cb379a77c5fc2d9d1503bdbe1154eed27d27b
iansalzgeberr <94173@sistemas.frc.utn.edu.ar>
2024-09-04 22:12:06 -0300

    api_venta

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const sequelize = require('./config/database');
 const Producto = require('./models/Producto');
 const productoRoutes = require('./routes/productoRoutes');
+const ventaRoutes = require('./routes/ventaRoutes');
 
 
 const app = express();
 app.use(express.json());
 app.use('/api', productoRoutes);
+app.use('/api', ventaRoutes);
 
 // Conexión y sincronización con la base de datos
 sequelize.sync()
